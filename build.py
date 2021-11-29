39147f98d3da178077e5862a1f30041c75bb8954
Nazar Kaznadzei <nazar.kaznadzei@amadeus.com>
2021-11-29 18:08:47 +0200

    In progress build.py

--- build.py
+++ build.py
@@ -17,6 +17,9 @@ def get_included_content():
 
     files_tree = ready_directory_and_build_info_tree(files_root_folder_path)
     for key, file_info in files_tree.items():
+        if included_content != '':
+            included_content += "\r\n"
+
         included_content += file_info['class_content']
 
     return included_content
@@ -49,10 +52,34 @@ def get_file_infos(file_path):
         file_infos[class_name] = {
             'path': file_path,
             'class_name': class_name,
-            'class_content': file_content
+            'class_content': get_class_content(class_name, file_content),
+            'dependencies': get_class_dependencies(class_name, file_content)
         }
 
     return file_infos
+
+
+def get_class_content(class_name, file_content):
+    class_content_regex_with_multi_classes = r'class {}(.*?):(.*)(class)'.format(class_name)
+    class_content_regex_with_one_class = r'class {}(.*?):(.*)'.format(class_name)
+
+    class_content = ''
+    find_class_content_result = re.search(class_content_regex_with_multi_classes, file_content, re.S)
+    if not find_class_content_result:
+        find_class_content_result = re.search(class_content_regex_with_one_class, file_content, re.S)
+        if find_class_content_result:
+            class_content = find_class_content_result.group(0)
+    else:
+        class_content = find_class_content_result.group(0)
+        # need to remove last class text
+        class_content = class_content[0:-5]
+
+    return class_content
+
+
+def get_class_dependencies(class_name, file_content):
+    dependencies = []
+
 
 
 if __name__ == '__main__':
