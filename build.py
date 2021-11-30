737861945d6841810b0f5af47d87c891ca32644d
Nazar Kaznadzei <nazar.kaznadzei@amadeus.com>
2021-11-30 17:05:19 +0200

    Finalize build.py

--- build.py
+++ build.py
@@ -15,23 +15,92 @@ def get_included_content():
     files_root_folder_path = './game'
     included_content = ''
 
+    collected_imports = read_directory_and_collect_imports(files_root_folder_path)
+    for collected_import in collected_imports:
+        if included_content != '':
+            included_content += "\n"
+
+        included_content += collected_import
+
     files_info_tree = read_directory_and_build_files_info_tree(files_root_folder_path)
     for file_info in files_info_tree:
         if included_content != '':
-            included_content += "\r\n"
+            included_content += "\r\n\n"
 
         included_content += file_info['class_content']
 
     return included_content
 
 
+def read_directory_and_collect_imports(base_path: str):
+    collected_imports = set()
+    for file_path in glob.iglob(base_path + '**/**', recursive=True):
+        if not is_applicable_file(file_path):
+            continue
+
+        imports_from_file = collect_imports_from_file(file_path)
+        for import_from_file in imports_from_file:
+            collected_imports.add(import_from_file)
+
+    return collected_imports
+
+
+def is_applicable_file(file_path: str) -> bool:
+    if file_path.find('.py') == -1:
+        return False
+
+    if file_path.find('__init__.py') > -1:
+        return False
+
+    return True
+
+
+def collect_imports_from_file(file_path: str) -> set:
+    collected_imports = set()
+
+    file_resource = open(file_path, 'r')
+    file_content = file_resource.read()
+    file_resource.close()
+
+    from_import_regex = r'^from (.*) import (.*)'
+    import_regex = r'^import (.*)'
+
+    from_import_result = re.findall(from_import_regex, file_content, re.MULTILINE)
+    import_result = re.findall(import_regex, file_content, re.MULTILINE)
+
+    for from_import_item in from_import_result:
+        if is_internal_import(from_import_item[0]):
+            continue
+
+        import_line = 'from {} import {}'.format(from_import_item[0], from_import_item[1])
+
+        collected_imports.add(import_line)
+
+    for import_item in import_result:
+        if is_internal_import(import_item):
+            continue
+
+        import_line = 'import {}'.format(import_item)
+
+        collected_imports.add(import_line)
+
+    return collected_imports
+
+
+def is_internal_import(import_line: str) -> bool:
+    internal_import_folders = ['entity']
+
+    for internal_import_folder in internal_import_folders:
+        if import_line.find(internal_import_folder) > -1:
+            return True
+
+    return False
+
+
 def read_directory_and_build_files_info_tree(base_path: str):
     files_info_tree = {}
     for file_path in glob.iglob(base_path + '**/**', recursive=True):
-        if file_path.find('.py') == -1:
-            continue
-
-        if file_path.find('__init__.py') > -1:
+        if not is_applicable_file(file_path):
             continue
 
         file_infos = get_file_infos(file_path)
@@ -78,8 +147,8 @@ def setup_dependencies(file_info, class_name_list):
             continue
 
         file_info['dependencies'].append(class_name)
-        
-        
+
+
 def update_position(files_info_tree: dict):
     is_position_updated = False
     for class_name, file_info in files_info_tree.items():
@@ -116,6 +185,8 @@ def get_class_content(class_name, file_content):
         class_content = find_class_content_result.group(0)
         # need to remove last class text
         class_content = class_content[0:-5]
+
+    class_content = class_content.strip()
 
     return class_content
 
